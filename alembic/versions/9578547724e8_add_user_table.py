b2e2fe05c51139ad63654ce8ac32b27dc32d0bf8
Cornelius443 <sundaycornelius55@gmail.com>
2025-02-18 20:44:56 +0100

    removed length for entry

--- alembic/versions/9578547724e8_add_user_table.py
+++ alembic/versions/9578547724e8_add_user_table.py
@@ -22,8 +22,8 @@ def upgrade():
     op.create_table( 
         'users',
         sa.Column('id', sa.Integer(), nullable=False),
-        sa.Column('email', sa.String(50), nullable=False),
-        sa.Column('password', sa.String(50), nullable=False),
+        sa.Column('email', sa.String(), nullable=False),
+        sa.Column('password', sa.String(), nullable=False),
         sa.Column('created_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
         sa.PrimaryKeyConstraint('id'),
         sa.UniqueConstraint('email')
